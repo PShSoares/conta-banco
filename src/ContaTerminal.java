ba93002373c93c9f98b03cd1584573f953a9c634
PedroCode2 <Pedro.hsoares131@gmail.com>
2024-04-24 21:00:20 -0700

    Desafio DIO, Banco via Terminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da sua Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por Favor, seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+               agencia + " , conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+            scanner.close();
+    }
+}
